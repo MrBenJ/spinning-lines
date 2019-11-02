7888dc98b149435c2a2776f82fe88f54f2acaed6
Ben Junya <bjunya@gmail.com>
2019-11-02 14:32:27 -0700

    Add lines as constant. Add color

--- static/script.js
+++ static/script.js
@@ -1,16 +1,46 @@
-let i = 0;
+/**
+ * CONFIG PARAMS
+ */
+const TOTAL_LINES = 200;
 const ROTATION_INCREMENT = 5;
-const lines = document.getElementsByClassName('line');
-const body = document.querySelector('.body');
-const total = lines.length - 1;
+const COLORS = [
+  "#B1DDF1",
+  "#0080FF",
+  "#008ECC",
+  "#0E4D92",
+  "#008081"
+];
 
+/**
+ * ELEMENTS
+ */
+const body = document.querySelector('.body');
+const circle = document.querySelector('.circle');
+
+
+/**
+ * CREATE ELEMENTS
+ */
+for (let l = 0; l < TOTAL_LINES; l++ ) {
+  const element = document.createElement('div');
+  element.classList.add('line');
+  circle.appendChild(element);
+}
+
+const lines = document.getElementsByClassName('line');
+const total = lines.length - 1;
+let i = 0;
 let rotation = 0;
+
 loop();
 function loop() {
   window.requestAnimationFrame(loop);
   const line = lines[i];
-  line.style = `transform: rotateZ(${rotation}deg)`;
+  const totalTurns = Math.floor(rotation / 360);
+  const colorIndex = Math.floor(totalTurns % COLORS.length);
+  line.style = `transform: rotateZ(${rotation}deg); background-color: ${COLORS[colorIndex]};`;
   
+
 
   if (i === total) {
     i = 0;
